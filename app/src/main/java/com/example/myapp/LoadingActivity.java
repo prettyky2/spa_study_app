b11c375033470ea73246231eab7f854f205fae03
prettyky2 <prettyky2@naver.com>
2025-01-07 16:11:27 +0900

    250107_function Update

--- app/src/main/java/com/example/myapp/LoadingActivity.java
+++ app/src/main/java/com/example/myapp/LoadingActivity.java
@@ -1,6 +1,10 @@
 package com.example.myapp;
 
+
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class LoadingActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG = "LoadingActivity";
+    Button startButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_loading_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //initialize object
+        startButton = findViewById(R.id.loading_page_start_button);
+        startButton.setOnClickListener(this);
+
+    } //onCreate();
+
+    @Override
+    public void onClick(View v) {
+        // 클릭된 뷰의 ID 확인
+        if (v.getId() == R.id.loading_page_start_button) {
+            Toast.makeText(this, "Start button clicked!", Toast.LENGTH_SHORT).show();
+        }
     }
+
+
 }
