4a97289f4ea349608f4b3eb77a4da8bc01010ff2
Jiyong Park Home <prettyky2@naver.com>
2025-01-07 21:49:57 +0900

    250107_Add MainPageActivity

--- app/src/main/java/com/example/myapp/LoadingActivity.java
+++ app/src/main/java/com/example/myapp/LoadingActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import android.widget.Toast;
+import android.content.Intent;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,7 +23,7 @@ public class LoadingActivity extends AppCompatActivity implements View.OnClickLi
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_loading_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loading_activity), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
@@ -39,6 +40,8 @@ public class LoadingActivity extends AppCompatActivity implements View.OnClickLi
         // 클릭된 뷰의 ID 확인
         if (v.getId() == R.id.loading_page_start_button) {
             Toast.makeText(this, "Start button clicked!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, MainPageActivity.class);
+            startActivity(intent);
         }
     }
 
